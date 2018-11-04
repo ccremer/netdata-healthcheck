dbe445cca41c576f22f21745036a71bb13883dc7
Chris <github.account@chrigel.net>
2018-11-04 21:45:04 +0100

    Improve error handling in build script

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@
 
 REPOSITORY="${REPOSITORY:-netdata-healthcheck}"
 
+fail_on_error() {
+    if [ ${1} -ne 0 ]; then
+        echo "------------------------ ${2}"
+        echo "Exiting with exit code ${1}"
+        exit ${1}
+    fi
+}
+
 if [ ${VERSION+x} ]; then
     VERSION="-${VERSION}"
 else
@@ -12,12 +20,21 @@ fi
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
+# Build images
 for ARCH in armhf amd64 i386 aarch64; do
-     docker build --build-arg ARCH="${ARCH}" --tag "${REPOSITORY}:${ARCH}${VERSION}" --file Dockerfile ./
+    image="${REPOSITORY}:${ARCH}${VERSION}"
+    echo "Building: ARCH=${ARCH}, tag=${image}"
+    docker build --build-arg ARCH="${ARCH}" --tag "${image}" .
+    fail_on_error $? "Error building ${image}, check the output above!"
 done
-docker tag "${REPOSITORY}:amd64" "${REPOSITORY}:latest"
 
-# Push images to registry
+# Tag latest
+image="${REPOSITORY}:latest"
+echo "Tag latest: ${REPOSITORY}:amd64"
+docker tag "${REPOSITORY}:amd64" "${image}"
+fail_on_error $? "Error tagging ${image}, check the output above!"
+
+# Login
 if [ -z ${DOCKER_USERNAME+x} ]; then
     echo "No docker hub username  specified. Exiting without pushing images to registry"
     exit 0
@@ -27,7 +44,18 @@ if [ -z ${DOCKER_PASSWORD+x} ]; then
     exit 0
 fi
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Push images
 for ARCH in amd64 armhf i386 aarch64; do
-    docker push "${REPOSITORY}:${ARCH}${VERSION}"
+    image="${REPOSITORY}:${ARCH}${VERSION}"
+    echo "Pushing image: ${image}"
+    docker push "${image}"
+    fail_on_error $? "Error pushing ${image}, check the output above!"
 done
-docker push "${REPOSITORY}:latest"
+
+# Push latest
+image="${REPOSITORY}:latest"
+echo "Pushing image: ${image}"
+docker push "${image}"
+fail_on_error $? "Error pushing ${image}, check the output above!"
+
